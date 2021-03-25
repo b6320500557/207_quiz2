4b832d22023ab315f243efbac1a3fde9c790d98d
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-03-25 18:45:08 +0700

    add 4

--- Silk Texture - 6320500557.c
+++ Silk Texture - 6320500557.c
@@ -4,13 +4,7 @@ int main()
     int n,m,i,j;
     scanf("%d",&n);
     scanf("%d",&m);
-    int silk[n][m],p[n],q[n],r[n];
-     for(i=1; i<=n; i++)
-    {
-        p[i] = 0;
-        q[i] = 0;
-        r[i] = 0;
-    }
+    int silk[n][m];
     for(i=1; i<=n; i++)
     {
         for(j=1; j<=m; j++)
@@ -18,18 +12,21 @@ int main()
             scanf("%d",&silk[i][j]);
         }
     }
-     for(i=1; i<=n; i++)
+    for(i=1; i<=n; i++)
     {
-        p[i] = silk[i];
-        q[i] = silk[i+1];
-        r[i] = silk[i+2];
-    }
-     for(i=1; i<=n; i++)
-    {
-        printf("%d ",p[i]);
-        printf("%d ",q[i]);
-        printf("%d ",r[i]);
-    }
+        for(j=1; j<=70; j++)
+        {
+            if(i == silk[i][j])
+            {
+                printf("x");
+            }
+            else
+            {
+               printf("o");
+            }
 
+        }
+        printf("\n");
+    }
     return 0;
 }
